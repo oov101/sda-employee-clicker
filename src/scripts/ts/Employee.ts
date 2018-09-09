0d76ed05dd87f129ec469b49a34fe0550cda4a69
Dawid Walenciej <dawid101w@gmail.com>
2018-09-09 18:45:42 +0200

    create and draw Employee from Employee Component

--- src/scripts/ts/Employee.ts
+++ src/scripts/ts/Employee.ts
@@ -3,19 +3,31 @@ import { EmployeeClicker } from 'EmployeeClicker';
 export class Employee {
     game: EmployeeClicker;
     intervalTime: number;
-    element: Element;
+    element: HTMLDivElement;
     intervalId: any;
+    map: Element;
     
-    constructor(element: Element, game: EmployeeClicker) {
+    constructor(game: EmployeeClicker) {
         this.game = game;
+        this.map = this.game.map.mapContainer;
         this.intervalTime = 5000;
-        this.element = element;
+        this.element = document.createElement('div');
         this.element.addEventListener('click', () => {
             this.click();
         });
         this.updateInterval();
+        this.drawEmployee();
     }
-    
+
+    drawEmployee() {
+        this.map.appendChild(this.element);
+        this.element.setAttribute('class', 'employee-frame-0');
+        this.element.style.zIndex = '1';
+        this.element.style.top = '106px';
+        this.element.style.left = '302px';
+        this.element.style.transform = 'rotate(8deg)';
+    }
+
     click() {
         this.game.addToScore(1);
         this.game.playAudio('employeeClick');
